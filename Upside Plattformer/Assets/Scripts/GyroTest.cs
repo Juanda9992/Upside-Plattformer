e8b21e13838489a5d7896d8b7a47618ad04bf0f6
Juanda9992 <juandavidgonzog1@gmail.com>
2022-05-05 15:19:18 -0500

    Build 1
    Added ball movement with phone rotation (BETA)
    
    Imported TMPro

--- Upside Plattformer/Assets/Scripts/GyroTest.cs
+++ Upside Plattformer/Assets/Scripts/GyroTest.cs
@@ -1,18 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GyroTest : MonoBehaviour
-{
+{   
+    [SerializeField]
+    private float currentSpeed, sensitivity;
+    private Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        Debug.Log(Screen.orientation);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        currentSpeed = Input.acceleration.x * sensitivity;
+        rb.AddForce(new Vector2(currentSpeed,0));
     }
 }
